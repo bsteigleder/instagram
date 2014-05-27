f0b889d6901d3ec3aa098f92830d432cceeb1b0a
unknown <alu201112030@PC-LAB404-62247.uniritter.local>
2014-05-27 09:52:40 -0300

    adicionando index e imagem

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3" >
+                    <img src="img/logo.png" class="col-md-offset-3" alt="" /><br />
+                    <a  href="https://api.instagram.com/oauth/authorize/?client_id=8dcfc9cf4d684b5baeaf00a053f28785&redirect_uri=http://www.lifein.com.br&response_type=code" class="btn btn-success btn-lg">Logar No instagram</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
